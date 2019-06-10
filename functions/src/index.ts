fe04d24e076bb15b79c73c00c9400d60eb1123f1
Narayan Gupta <narayangupta@Narayans-MacBook-Air.local>
2019-06-10 16:59:51 -0400

    Initial setup

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+
+export const webApi = functions.https.onRequest(main);
+
+app.get('/warmup', (request, response) => {
+
+    response.send('Warming up friend.');
+
+})
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
